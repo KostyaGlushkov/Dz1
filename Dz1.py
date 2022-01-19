832b41e2ae4904531780d920ace37e1d517af1ef
KostyaGlushkov <Kostia2266@gmail.com>
2022-01-19 23:43:56 +0200

    first commit

---
+++ Dz1.py
@@ -0,0 +1,22 @@
+first = 10
+second = 30
+sum_of_int1 = first + second
+sum_of_int2 = first - second
+sum_of_int3 = first * second
+sum_of_int4 = first / second
+sum_of_int5 = first ** second
+
+print(sum_of_int1)
+print(sum_of_int2)
+print(sum_of_int3)
+print(sum_of_int4)
+print(sum_of_int5)
+
+c = 10 % 30
+print(c)
+C = 10 // 30
+print(C)
+
+# Ответы на вопросы
+# Учусь в 11 классе
+# попробовать себя в программирование чтоб определиться с выбором будущей профессии
